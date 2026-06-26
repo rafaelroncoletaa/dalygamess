4329ceaead74d2fdc91cf5fb4db8d2a59cc355b4
Rafael <rafaelnh089@hotmail.com>
2026-06-25 21:06:39 -0300

    Criando página search

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,41 @@
+import { GameProps } from "@/utils/types/game";
+import Container from "@/components/container";
+import Input from "@/components/input";
+import GameCard from "@/components/GameCard";
+
+async function getData(title: string) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`,
+    );
+    return res.json();
+  } catch (err) {
+    return err;
+  }
+}
+
+export default async function Search({
+  params: { title },
+}: {
+  params: { title: string };
+}) {
+  const games: GameProps[] = await getData(title);
+
+  return (
+    <main className="w-full text-black">
+      <Container>
+        <Input />
+
+        <h1 className="font-bold text-xl mt-8 mb-5">
+          Veja o que encontramos na nossa base
+        </h1>
+
+        {!games && <p>Esse jogo não existe</p>}
+
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {games && games.map((item) => <GameCard key={item.id} data={item} />)}
+        </section>
+      </Container>
+    </main>
+  );
+}
